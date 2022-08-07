06003e05e78e7b1b4588042b8cc37858f90474b2
Fidyay <fidiy1878@gmail.com>
2022-08-07 23:37:27 +0300

    initial commit

---
+++ index.ts
@@ -0,0 +1,3 @@
+export const a = (n: number): number => ++n
+
+console.log(a(1))
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const devMode = process.argv[process.argv.indexOf('--mode') + 1] !== 'production'
+
+module.exports = {
+    entry: './index.ts',
+    mode: devMode ? 'development' : 'production',
+    watch: devMode,
+    output: {
+        clean: true,
+        library: {
+            name: "decibel",
+            type: 'umd'
+        },        
+        filename: 'index.js'
+    },
+    module: {
+        rules: [
+            {
+              test: /\.(t|j)s$/,
+              use: ['babel-loader', 'ts-loader']
+            }
+        ]
+    }
+}
