aff3b17dc0f06ca5df95776ff9436152f5be334a
Fidyay <fidiy1878@gmail.com>
2022-09-06 01:29:49 +0300

    added ignore patterns in eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
         "browser": true,
         "es2021": true
     },
+    "ignorePatterns": ["dist", "coverage"],
     "extends": [
         "eslint:recommended",
         "plugin:@typescript-eslint/recommended"
