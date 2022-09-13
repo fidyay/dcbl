dfbbe6c76d05bd178cd6536df5373351dbdfe427
Fidyay <fidiy1878@gmail.com>
2022-09-14 01:38:50 +0300

    initial commit for virtual dom

--- index.ts
+++ index.ts
@@ -1,2 +1,3 @@
 export { default as ClassComponent } from "./ClassComponent/ClassComponent";
 export { default as createElement } from "./createElement/createElement";
+export { default as VirtualDOM } from "./VirtualDOM/VirtualDOM";
