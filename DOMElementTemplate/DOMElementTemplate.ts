cfac3707c31e7cc0cc4a93ea927399ed491fc611
Fidyay <fidiy1878@gmail.com>
2023-04-11 21:27:58 +0300

    added documentation for DOMElementTemplate class

--- DOMElementTemplate/DOMElementTemplate.ts
+++ DOMElementTemplate/DOMElementTemplate.ts
@@ -5,11 +5,24 @@ import type {
 export type DOMElementPropsType<T extends DOMElementType> =
   JSX.IntrinsicElements[T] & { children?: childrenType[] };
 
+/**
+ * A class specified for a template of a DOM element used later in virtual DOM to implement or change actual DOM element
+ *
+ * @field indexInParent - index in child nodes of parent DOM element
+ * @field DOMEl - link to a DOM element created by virtual DOM
+ * @field type - a tag name
+ * @field props - props of an element
+ */
 class DOMElementTemplate<T extends DOMElementType> {
   public indexInParent!: number;
   public DOMEl!: HTMLElement;
   public type: T;
   public props: DOMElementPropsType<T>;
+  /**
+   * Creates a {@link DOMElementTemplate} instance.
+   * @param type - a tag name
+   * @param props - props object
+   */
   constructor(type: T, props: DOMElementPropsType<T> = {}) {
     this.type = type;
     this.props = props;
