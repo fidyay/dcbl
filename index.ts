f8d4bf4463aa56331fed57d6c82e399868e138ce
Fidyay <fidiy1878@gmail.com>
2023-04-24 21:08:48 +0300

    everything is ready for generating docs

---
+++ index.ts
@@ -0,0 +1,14 @@
+import "./global";
+import Component from "./Components/Component";
+import ComponentManager from "./Components/ComponentManager";
+import createElement from "./createElement/createElement";
+import DOMElementTemplate from "./DOMElementTemplate/DOMElementTemplate";
+import VirtualDOM from "./VirtualDOM/VirtualDOM";
+
+export {
+  Component,
+  ComponentManager,
+  createElement,
+  DOMElementTemplate,
+  VirtualDOM
+};
